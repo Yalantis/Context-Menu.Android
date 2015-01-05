8d87b7b703da97ff6cc7d599fca758f4a511da6b
Kirill Penzykov <kirill.penzykov@yalantis.com>
2015-01-05 12:44:19 +0200

    added parcelable implementation

--- app/src/main/java/com/yalantis/dropdownmenu/Utils.java
+++ app/src/main/java/com/yalantis/dropdownmenu/Utils.java
@@ -18,28 +18,28 @@ import android.widget.TextView;
  */
 public class Utils {
 
-    public static int getDefaultActionBarSize(Context context){
+    public static int getDefaultActionBarSize(Context context) {
         final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
-                new int[] { android.R.attr.actionBarSize });
+                new int[]{android.R.attr.actionBarSize});
         int actionBarSize = (int) styledAttributes.getDimension(0, 0);
         styledAttributes.recycle();
         return actionBarSize;
     }
 
-    public static TextView getItemTextView(Context context, String title, int menuItemSize){
+    public static TextView getItemTextView(Context context, String title, int menuItemSize) {
         TextView itemTextView = new TextView(context);
         RelativeLayout.LayoutParams textLayoutParams = new RelativeLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT, menuItemSize);
         itemTextView.setLayoutParams(textLayoutParams);
-        itemTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX,context.getResources().getDimension(R.dimen.menu_text_size));
+        itemTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, context.getResources().getDimension(R.dimen.menu_text_size));
         itemTextView.setTextColor(context.getResources().getColor(android.R.color.white));
         itemTextView.setText(title);
-        itemTextView.setPadding(0,0,(int)context.getResources().getDimension(R.dimen.text_right_padding),0);
+        itemTextView.setPadding(0, 0, (int) context.getResources().getDimension(R.dimen.text_right_padding), 0);
         itemTextView.setGravity(Gravity.CENTER_VERTICAL);
         return itemTextView;
     }
 
-    public static ImageView getItemImageButton(Context context, Drawable drawable){
+    public static ImageView getItemImageButton(Context context, Drawable drawable) {
         ImageView imageView = new ImageButton(context);
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -57,9 +57,9 @@ public class Utils {
         return imageView;
     }
 
-    public static View getDivider(Context context){
+    public static View getDivider(Context context) {
         View dividerView = new View(context);
-        RelativeLayout.LayoutParams viewLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,(int)context.getResources().getDimension(R.dimen.divider_height));
+        RelativeLayout.LayoutParams viewLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) context.getResources().getDimension(R.dimen.divider_height));
         viewLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
         dividerView.setLayoutParams(viewLayoutParams);
         dividerView.setClickable(true);
@@ -67,13 +67,13 @@ public class Utils {
         return dividerView;
     }
 
-    public static RelativeLayout getImageWrapper(Context context, int menuItemSize, Drawable drawable, View.OnClickListener onCLick){
+    public static RelativeLayout getImageWrapper(Context context, int menuItemSize, int drawableId, View.OnClickListener onCLick) {
         RelativeLayout imageWrapper = new RelativeLayout(context);
         LinearLayout.LayoutParams imageWrapperLayoutParams = new LinearLayout.LayoutParams(menuItemSize, menuItemSize);
         imageWrapper.setLayoutParams(imageWrapperLayoutParams);
         imageWrapper.setBackgroundColor(context.getResources().getColor(R.color.menu_item_background));
         imageWrapper.setOnClickListener(onCLick);
-        imageWrapper.addView(Utils.getItemImageButton(context, drawable));
+        imageWrapper.addView(Utils.getItemImageButton(context, context.getResources().getDrawable(drawableId)));
         imageWrapper.addView(getDivider(context));
         return imageWrapper;
     }
