83a9e4e9bccd34ed4458bf37e62bd0cce9e23653
aleksandr.zaycev <aleksandr.zaycev@yalantis.com>
2015-01-27 10:28:09 +0200

    add OnMenuItemLongClickListener

---
+++ lib/src/main/java/com/yalantis/contextmenu/lib/interfaces/OnItemLongClickListener.java
@@ -0,0 +1,11 @@
+package com.yalantis.contextmenu.lib.interfaces;
+
+import android.view.View;
+
+/**
+ * Menu adapter item long click listener
+ */
+public interface OnItemLongClickListener {
+
+    public void onLongClick(View v);
+}
---
+++ lib/src/main/java/com/yalantis/contextmenu/lib/interfaces/OnMenuItemClickListener.java
@@ -0,0 +1,11 @@
+package com.yalantis.contextmenu.lib.interfaces;
+
+import android.view.View;
+
+/**
+ * Menu item click listener
+ */
+public interface OnMenuItemClickListener {
+
+    public void onMenuItemClick(View clickedView, int position);
+}
