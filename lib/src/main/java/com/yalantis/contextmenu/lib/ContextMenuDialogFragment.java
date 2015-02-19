0b65c61ff8e67e350bdd1a427b57e7a020cc5496
Alexander Zaitsev <madeinussr91@gmail.com>
2015-02-19 11:55:16 +0200

    Add possibility to interact with translucent theme

--- lib/src/main/java/com/yalantis/contextmenu/lib/ContextMenuDialogFragment.java
+++ lib/src/main/java/com/yalantis/contextmenu/lib/ContextMenuDialogFragment.java
@@ -1,6 +1,8 @@
 package com.yalantis.contextmenu.lib;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.DialogFragment;
@@ -26,6 +28,8 @@ public class ContextMenuDialogFragment extends DialogFragment implements OnItemC
     private static final String MENU_OBJECTS = "menu_objects";
     private static final String ANIMATION_DELAY = "animation_delay";
     private static final String ANIMATION_DURATION = "animation_duration";
+    private static final String FITS_SYSTEM_WINDOW = "fits_system_window";
+    private static final String CLIP_TO_PADDING = "clip_to_padding";
 
     private LinearLayout mWrapperButtons;
     private LinearLayout mWrapperText;
@@ -34,7 +38,10 @@ public class ContextMenuDialogFragment extends DialogFragment implements OnItemC
     private int mActionBarHeight;
     private OnMenuItemClickListener mItemClickListener;
     private OnMenuItemLongClickListener mItemLongClickListener;
-    private int mAnimationDelay = 0; // delay after opening and before closing dialogfragent
+    /**
+     * Delay after opening and before closing {@link com.yalantis.contextmenu.lib.ContextMenuDialogFragment}
+     */
+    private int mAnimationDelay = 0;
     private int mAnimationDuration;
 
     public static ContextMenuDialogFragment newInstance(int actionBarSize, List<MenuObject> menuObjects) {
@@ -46,23 +53,39 @@ public class ContextMenuDialogFragment extends DialogFragment implements OnItemC
         return contextMenuDialogFragment;
     }
 
-    public static ContextMenuDialogFragment newInstance(int actionBarSize, ArrayList<MenuObject> menuObjects, int animationDelay) {
+    public static ContextMenuDialogFragment newInstance(int actionBarSize, List<MenuObject> menuObjects, int animationDelay) {
         ContextMenuDialogFragment contextMenuDialogFragment = new ContextMenuDialogFragment();
         Bundle args = new Bundle();
         args.putInt(ACTION_BAR_SIZE, actionBarSize);
-        args.putParcelableArrayList(MENU_OBJECTS, menuObjects);
+        args.putParcelableArrayList(MENU_OBJECTS, new ArrayList<>(menuObjects));
         args.putInt(ANIMATION_DELAY, animationDelay);
         contextMenuDialogFragment.setArguments(args);
         return contextMenuDialogFragment;
     }
 
-    public static ContextMenuDialogFragment newInstance(int actionBarSize, ArrayList<MenuObject> menuObjects, int animationDelay, int animationDuration) {
+    public static ContextMenuDialogFragment newInstance(int actionBarSize, List<MenuObject> menuObjects, int animationDelay, int animationDuration) {
         ContextMenuDialogFragment contextMenuDialogFragment = new ContextMenuDialogFragment();
         Bundle args = new Bundle();
         args.putInt(ACTION_BAR_SIZE, actionBarSize);
-        args.putParcelableArrayList(MENU_OBJECTS, menuObjects);
+        args.putParcelableArrayList(MENU_OBJECTS, new ArrayList<>(menuObjects));
         args.putInt(ANIMATION_DELAY, animationDelay);
         args.putInt(ANIMATION_DURATION, animationDuration);
+        contextMenuDialogFragment.setArguments(args);
+        return contextMenuDialogFragment;
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public static ContextMenuDialogFragment newInstance(int actionBarSize, List<MenuObject> menuObjects,
+                                                        int animationDelay, int animationDuration,
+                                                        boolean fitsSystemWindow, boolean clipToPadding) {
+        ContextMenuDialogFragment contextMenuDialogFragment = new ContextMenuDialogFragment();
+        Bundle args = new Bundle();
+        args.putInt(ACTION_BAR_SIZE, actionBarSize);
+        args.putParcelableArrayList(MENU_OBJECTS, new ArrayList<>(menuObjects));
+        args.putInt(ANIMATION_DELAY, animationDelay);
+        args.putInt(ANIMATION_DURATION, animationDuration);
+        args.putBoolean(FITS_SYSTEM_WINDOW, fitsSystemWindow);
+        args.putBoolean(CLIP_TO_PADDING, clipToPadding);
         contextMenuDialogFragment.setArguments(args);
         return contextMenuDialogFragment;
     }
@@ -102,6 +125,14 @@ public class ContextMenuDialogFragment extends DialogFragment implements OnItemC
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        if (getArguments().containsKey(FITS_SYSTEM_WINDOW)) {
+            rootView.setFitsSystemWindows(getArguments().getBoolean(FITS_SYSTEM_WINDOW));
+        }
+        if (getArguments().containsKey(CLIP_TO_PADDING)) {
+            ((ViewGroup) rootView).setClipToPadding(getArguments().getBoolean(CLIP_TO_PADDING));
+        }
+
         initViews(rootView);
         getDialog().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         initDropDownMenuAdapter();
